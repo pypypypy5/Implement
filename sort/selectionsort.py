f41295b68060cc5c54281213cb9c253725f7b157
pypypypy5 <142680357+pypypypy5@users.noreply.github.com>
2024-05-01 01:26:59 +0900

    Update selectionsort.py

--- sort/selectionsort.py
+++ sort/selectionsort.py
@@ -1,19 +1,7 @@
-import sys
-input = sys.stdin.readline
-
-li = []
-
-num_ofn = int(input())
-for i in range(num_ofn):
-    num = int(input())
-    li.append(num)
-
-for index_now in range(1,len(li)):
-    for i in range(index_now,0,-1):
-        if li[i] < li[i-1]:
-            li[i], li[i-1] = li[i-1], li[i]
-        else:
-            break
-
-for i in li:
-    print(i)
+def selectionsort(li):
+    for index_now in range(1,len(li)):
+        for i in range(index_now,0,-1):
+            if li[i] < li[i-1]:
+                li[i], li[i-1] = li[i-1], li[i]
+            else:
+                break
